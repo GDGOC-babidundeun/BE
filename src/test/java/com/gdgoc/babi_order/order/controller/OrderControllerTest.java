e39e3c7c5ca92487703a0c8ba4c669d5dd95cd95
김민준 <minjun0306@hufs.ac.kr>
2026-07-25 22:58:44 +0900

    feat: 관리자 로그인 및 JWT 인증 구현

--- src/test/java/com/gdgoc/babi_order/order/controller/OrderControllerTest.java
+++ src/test/java/com/gdgoc/babi_order/order/controller/OrderControllerTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(OrderController.class)
 @Import({SecurityConfig.class, CorsProperties.class, OrderExceptionHandler.class})
+@WithMockUser(roles = "ADMIN")
 class OrderControllerTest {
 
     @Autowired
